9853fe69c91269b761cae6a12cf0c151bbd02a84
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-06 21:24:58 +0530

    Categorie card added

--- src/Component/Coursel.jsx
+++ src/Component/Coursel.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Product from './Product'
 import { useRef } from 'react';
-import img1 from '/Coursel Img/img1.jpg'
-import img2 from '/Coursel Img/img2.jpg'
-import img3 from '/Coursel Img/img3.jpg'
-import img4 from '/Coursel Img/img4.png'
-import img5 from '/Coursel Img/img5.jpeg'
-import img6 from '/Coursel Img/img6.jpg'
-import img7 from '/Coursel Img/img7.webp'
-import img8 from '/Coursel Img/img8.jpg'
-import img9 from '/Coursel Img/img9.avif'
-import img10 from '/Coursel Img/img10.png'
+// import img1 from '/Coursel Img/img1.jpg'
+// import img2 from '/Coursel Img/img2.jpg'
+// import img3 from '/Coursel Img/img3.jpg'
+// import img4 from '/Coursel Img/img4.png'
+// import img5 from '/Coursel Img/img5.jpeg'
+// import img6 from '/Coursel Img/img6.jpg'
+// import img7 from '/Coursel Img/img7.webp'
+// import img8 from '/Coursel Img/img8.jpg'
+// import img9 from '/Coursel Img/img9.avif'
+// import img10 from '/Coursel Img/img10.png'
 function Coursel({num}) {
     const cards = [
         {
@@ -18,70 +18,70 @@ function Coursel({num}) {
             title: "Introduction to Web Development",
             professorName: "Dr. Sarah Johnson",
             price: "$29.99",
-            img: img1
+            img: '/Coursel Img/img1.jpg'
         },
         {
             id: 2,
             title: "Data Science Fundamentals",
             professorName: "Prof. Michael Brown",
             price: "$39.99",
-            img: img2
+            img: '/Coursel Img/img2.jpg'
         },
         {
             id: 3,
             title: "Mobile App Development Workshop",
             professorName: "Dr. Alex Rodriguez",
             price: "$49.99",
-            img: img3
+            img: '/Coursel Img/img3.jpg'
         },
         {
             id: 4,
             title: "Algorithms and Data Structures",
             professorName: "Prof. Emily Williams",
             price: "$34.99",
-            img:img4
+            img:'/Coursel Img/img4.png'
         },
         {
             id: 5,
             title: "Machine Learning Masterclass",
             professorName: "Dr. David Lee",
             price: "$59.99",
-            img:img5
+            img:'/Coursel Img/img5.jpeg'
         },
         {
             id: 6,
             title: "Digital Marketing Strategies",
             professorName: "Prof. Karen Thompson",
             price: "$44.99",
-            img:img6
+            img:'/Coursel Img/img6.jpg'
         },
         {
             id: 7,
             title: "Cybersecurity Essentials",
             professorName: "Dr. Richard Miller",
             price: "$54.99",
-            img:img7
+            img:'/Coursel Img/img7.webp'
         },
         {
             id: 8,
             title: "Blockchain Technology Explained",
             professorName: "Prof. Christopher Turner",
             price: "$39.99",
-            img: img8
+            img: '/Coursel Img/img8.jpg'
         },
         {
             id: 9,
             title: "Game Development Workshop",
             professorName: "Dr. Jessica Martinez",
             price: "$49.99",
-            img: img9
+            img: '/Coursel Img/img9.avif'
         },
         {
             id: 10,
             title: "Artificial Intelligence Foundations",
             professorName: "Prof. Benjamin Davis",
             price: "$79.99",
-            img:img10
+            img:'/Coursel Img/img10.png'
         },
     ];
     const container=useRef();
