d74d71d07ce969c12bcbdcd796bf12a17077446b
SURYAWANSHI-VAIBHAV <144354322+SURYAWANSHI-VAIBHAV@users.noreply.github.com>
2024-02-11 11:58:39 +0530

    Update SignUp.jsx

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -4,6 +4,7 @@ function SignUp() {
   return (
     <div className='h-screen flex flex-col w-100 items-center justify-center'>
      <h1 className='text-center'>Sign Up</h1> 
+      <p>hi this is my changes</p>
     </div>
   )
 }
